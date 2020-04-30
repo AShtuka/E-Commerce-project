f6d8ed6c2d4c6131fa844c8f7751a51e35c9f40a
Oleksandr Shtuka <ashtuka@gmail.com>
2020-04-30 11:24:01 +0300

    start shop header view

--- client/src/components/shop-header/shop-header.js
+++ client/src/components/shop-header/shop-header.js
@@ -12,14 +12,39 @@ const ShopHeader = () => {
         <nav>
             <div className="nav-wrapper">
                 <Link to='/' className="brand-logo">Logo</Link>
-                <ul className="right">
-                    <li  className='cart'>
+                <ul className="hide-on-med-and-down header-center-block">
+                    <li>
+                        <Link to="/cart">CATALOG</Link>
+                    </li>
+                    <li>
+                        <Link to="/cart">DELIVERY</Link>
+                    </li>
+                    <li>
+                        <Link to="/cart">BLOG</Link>
+                    </li>
+                    <li>
+                        <Link to="/cart">CONTACT</Link>
+                    </li>
+                </ul>
+                <ul className="right hide-on-med-and-down vertical-alight">
+                    <li className='cart'>
+                        <Link to="/cart">
+                            <span className="material-icons">favorite</span>
+                        </Link>
+                    </li>
+                </ul>
+                <ul className="right vertical-alight">
+                    <li>
+                        <Link to="/cart">
+                            <span className="material-icons">search</span>
+                        </Link>
+                    </li>
+                    <li className='cart'>
                         <Link to="/cart">
                             <span className="material-icons">shopping_cart</span>
                             <span className='cart-badge'>{cartItemsAmount}</span>
                         </Link>
                     </li>
-
                 </ul>
             </div>
         </nav>
