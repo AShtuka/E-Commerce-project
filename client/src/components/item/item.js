adeee10fe919902c37d40f62afebbba7b30b528e
Oleksandr Shtuka <ashtuka@gmail.com>
2020-05-02 11:28:08 +0300

    start shopping page view

--- client/src/components/item/item.js
+++ client/src/components/item/item.js
@@ -12,14 +12,28 @@ const Item = ({item}) => {
             <div className="item-container">
                 <div className="card hoverable">
                     <div className="card-image">
-                        <img src={imgUrl} alt='NOT FOUND' />
+                        <img src={imgUrl[0]} alt='NOT FOUND' />
                     </div>
-                    <div className="card-content center-align">
-                        <span className="card-title">{title}</span>
-                        <p>{price} $</p>
-                    </div>
-                    <div className="card-action center-align cursor">
-                        <div onClick={() => dispatch(itemAddedToCart(item))}>Add to cart</div>
+                    <div className="card-content">
+                        <div className='evaluative-container'>
+                            <div className='stars'>
+                                <span className="material-icons">star</span>
+                                <span className="material-icons">star</span>
+                                <span className="material-icons">star</span>
+                                <span className="material-icons">star_half</span>
+                                <span className="material-icons">star_outline</span>
+                            </div>
+                            <div>
+                                <span className="material-icons">favorite_outline</span>
+                            </div>
+                        </div>
+                        <div className='info-container'>
+                            <span className="card-title">{title}</span>
+                            <p className='price'>{price} $</p>
+                            <div className='buy-btn-container'>
+                                <div className='buy-btn' onClick={() => dispatch(itemAddedToCart(item))}>ADD TO CART</div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
--- server.js
+++ server.js
@@ -12,19 +12,28 @@ const data = [
         id: 1,
         title: 'BLASTER NERF ELITE DISRUPTOR',
         price: 35,
-        imgUrl: 'https://nerf.com.ua/wp-content/uploads/2017/04/test-2.jpg'
+        imgUrl: [
+            'https://nerf.com.ua/wp-content/uploads/2017/04/test-2.jpg',
+            'https://nerf.com.ua/wp-content/uploads/2017/04/nerf_disruptor_2-1.jpg'
+        ]
     },
     {
         id: 2,
         title: 'BLASTER NERF ELITE SURGEFIRE',
         price: 75,
-        imgUrl: 'https://nerf.com.ua/wp-content/uploads/2018/02/E0011.jpg'
+        imgUrl: [
+            'https://nerf.com.ua/wp-content/uploads/2018/02/E0011.jpg',
+            'https://nerf.com.ua/wp-content/uploads/2018/02/E0011-2-1.jpg'
+        ]
     },
     {
         id: 3,
         title: 'Nerf Fortnite Super Soaker',
         price: 15,
-        imgUrl: 'https://nerf.com.ua/wp-content/uploads/2019/06/HC-E-2-600x600.jpg'
+        imgUrl: [
+            'https://nerf.com.ua/wp-content/uploads/2019/06/HC-E-2-600x600.jpg',
+            'https://nerf.com.ua/wp-content/uploads/2019/09/E7515-2.jpg'
+        ]
     },
 ];
 
